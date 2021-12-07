888527e25597418afcb1aed7bc4838b2b3ecaa0d
fragferir <48501068+fragferir@users.noreply.github.com>
2021-12-07 17:03:06 -0300

    PantJuego testeo
    añadida PantJuego para probar

---
+++ MonochromeRunnerlibGDX/README.md
@@ -0,0 +1,2 @@
+# MonochromeRunner, libGDX
+Proyecto de libGDX, un runner 1v1 con 5 niveles
---
+++ MonochromeRunnerlibGDX/core/src/com/monorun/game/MonoRun.java
@@ -0,0 +1,35 @@
+package com.monorun.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.monorun.pantallas.PantMenuPrincipal;
+
+public class MonoRun extends Game {
+	   public SpriteBatch batch;
+	   public BitmapFont font;
+	   public static int alto = 600;
+	   public static int ancho = 800;
+	   
+		@Override
+		public void create() {
+			
+			batch = new SpriteBatch();
+			font = new BitmapFont();
+			this.setScreen(new PantMenuPrincipal(this));
+		}
+		public void render() {
+			super.render();
+		}
+		public void dispose() {
+			batch.dispose();
+			font.dispose();
+		}
+}
