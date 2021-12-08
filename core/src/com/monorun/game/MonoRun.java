315d7f9150cc7d9ce955fcad7b5364c058fd43a3
fragferir <48501068+fragferir@users.noreply.github.com>
2021-12-08 19:58:15 -0300

    interfaz y pantalla de juego inicial
    añadida la interfaz (un cronometro) y modificada la pantalla de juego

--- core/src/com/monorun/game/MonoRun.java
+++ core/src/com/monorun/game/MonoRun.java
@@ -10,13 +10,13 @@ import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.Viewport;
+import com.monorun.pantallas.PantMenuPrincipal;
 
 public class MonoRun extends Game {
-	   SpriteBatch batch;
-	   BitmapFont font;
+	   public SpriteBatch batch;
+	   public BitmapFont font;
 	   public static int alto = 600;
 	   public static int ancho = 800;
-	   
 		@Override
 		public void create() {
 			
